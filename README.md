6a3350aa28ae994c4bf9e6ed5d5f63a8997afed2
jakdev047 <shuvo047niter@gmail.com>
2020-05-19 16:47:29 +0600

    basic command line action

---
+++ README.md
@@ -0,0 +1,17 @@
+## Available Scripts
+
+In the project directory, you can run:
+
+### `node serve`
+
+Runs the app in the development mode.<br />
+
+### `node serve -h`
+
+Runs the app in the development help mode.<br />
+
+### `node server add -f yourFirstName -l yourLastName -e yourEmail`
+### `node server add -f yourFirstName -l yourLastName -e yourEmail -t yourContactType`
+
+to add single contact runs the app in the development.<br />
+Contact type for example Personal .<br />
--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log('done')
+const program = require('commander');
+
+program.version('5.1.0');
+program.description('A command line Application');
+
+program.command('add')
+        .alias('a')
+        .description('to add data by command line')
+        .requiredOption('-f,--firstName <fName>','Type Your First Name')
+        .requiredOption('-l,--lastName <lName>','Type Your Last Name')
+        .requiredOption('-e,--email <email>','Type Your Email')
+        .option('-t,--type <type>','Type Your Contact','Personal')
+        .action(argv=>{
+          console.log(argv.firstName,argv.lastName,argv.email,argv.type);
+        });
+
+if(!process.argv[2]) {
+  program.help();
+}
+
+
+program.parse(process.argv);
+
+// node server add -f Jubayer -l Khan -e shuvo047niter@gmail.com
